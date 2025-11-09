d2243d365e8498a38fa4e6713f4d3723e7903126
Aymon <215028292+aymonix@users.noreply.github.com>
2025-11-09 14:09:35 +0000

    fix(ufwcheck): Correct syntax for readonly associative array

--- ufwcheck.sh
+++ ufwcheck.sh
@@ -28,7 +28,7 @@ set -euo pipefail
 # ==============================================================================
 readonly CMD_NAME=$(basename "$0")
 readonly CMD_ARGS="$CMD_NAME $*"
-readonly declare -A MONTH_MAP=(
+declare -r -A MONTH_MAP=(
   [Jan]=01 [Feb]=02 [Mar]=03 [Apr]=04 [May]=05 [Jun]=06
   [Jul]=07 [Aug]=08 [Sep]=09 [Oct]=10 [Nov]=11 [Dec]=12
 )
